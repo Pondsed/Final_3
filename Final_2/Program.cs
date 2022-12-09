aa178d9685b2302db98fd63f3e7f2edf4de186fa
Pondsed <111563844+Pondsed@users.noreply.github.com>
2022-12-09 15:55:43 +0700

    Add files via upload

---
+++ Final_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<string>LateFileList = new Queue<string>();
+        while(true){
+            Console.WriteLine("Please input your file name");
+            Console.WriteLine("Or Type Exit To Finish ");
+            Console.Write("Input : ");
+            string Input_FileName = Console.ReadLine();
+            if(Input_FileName == "Exit" ||Input_FileName == "exit" )
+            {
+                Console.WriteLine("---------------------- ");
+                break;
+            }
+            else{
+                if(LateFileList.GetLength() == 0){
+                    LateFileList.Push(Input_FileName);
+                }
+                else if(LateFileList.GetLength() > 0){
+                    int i = 0;
+                    while(true){
+                        if(Input_FileName == LateFileList.Get(i)){
+                            break;
+                        }
+                        if(Input_FileName != LateFileList.Get(i)){
+                            if(i == LateFileList.GetLength() - 1){
+                            LateFileList.Push(Input_FileName);
+                            if(LateFileList.GetLength() > 3){
+                                LateFileList.Pop();
+                            }
+                            break;
+                        }
+                            i++;
+                        }
+                    }
+                }
+            }
+        }
+        Console.WriteLine("Your late File list");
+        for(int i = 0; i<LateFileList.GetLength();i++){
+            Console.WriteLine(LateFileList.Get(i));
+        }
+        
+    }
+}
